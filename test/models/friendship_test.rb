cc1cf550af75d000dce32f6e5b9b5392776c9363
Dan K <itsdanya@gmail.com>
2016-07-14 16:40:14 +0300

    Annotate friendship test

--- test/models/friendship_test.rb
+++ test/models/friendship_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: friendships
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  friend_id  :integer
+#  accepted   :boolean          default(FALSE)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class FriendshipTest < ActiveSupport::TestCase
