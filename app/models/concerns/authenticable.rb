fc88e317d7eea955529ef0a98962e2914b330cb1
Dan K <itsdanya@gmail.com>
2016-07-14 03:23:01 +0300

    Add Facebook auth

---
+++ app/models/concerns/authenticable.rb
@@ -0,0 +1,18 @@
+module Authenticable
+  extend ActiveSupport::Concern
+
+  FACEBOOK_FIELDS = ['id', 'first_name', 'last_name', 'gender', 'birthday', 'picture.type(large)']
+
+  module ClassMethods
+    def find_or_initialize_by_facebook(graph)
+      facebook_user = graph.get_object('me', fields: FACEBOOK_FIELDS)
+      user = find_by(facebook_id: facebook_user['id'])
+
+      user || new(
+        first_name: facebook_user['first_name'], last_name: facebook_user['last_name'],
+        gender: facebook_user['gender'], facebook_id: facebook_user['id']
+        # remote_avatar_url: facebook_user.dig('picture', 'data', 'url').to_s
+      )
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :users, only: [:index, :create, :update] do
+      post 'authenticate_facebook', on: :collection
+    end
+  end
 end
